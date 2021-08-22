59dfc0aa7bbd0354b8dded9039708b1ba79b83e9
nickamorg <nickamorg0@gmail.com>
2021-08-22 13:54:23 +0300

    World & Continent Screens

--- lib/screens/ContinentScreen.dart
+++ lib/screens/ContinentScreen.dart
@@ -1,20 +1,28 @@
 import 'package:flagscoloring_pixelart/library.dart';
-import 'package:flagscoloring_pixelart/screens/ContinentScreen.dart';
+import 'package:flagscoloring_pixelart/screens/GameScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flagscoloring_pixelart/AppTheme.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class WorldScreen extends StatelessWidget {
+class ContinentScreen extends StatelessWidget {
+    final String continentTitle;
 
-    WorldScreen({ Key? key}) : super(key: key);
+    ContinentScreen({ Key? key, required this.continentTitle}) : super(key: key);
 
 	@override
 	Widget build(BuildContext context) {
-        return World();
+        return Continent(continentTitle: continentTitle);
 	}
 }
 
-class WorldState extends State<World> {
+class ContinentState extends State<Continent> {
+    String continentTitle = '';
+
+    @override
+    void initState() {
+        super.initState();
+
+        continentTitle = widget.continentTitle;
+    }
 
 	@override
     Widget build(BuildContext context) {
@@ -24,7 +32,7 @@ class WorldState extends State<World> {
                     color: AppTheme.MAIN_COLOR,
                     image: DecorationImage(
                         colorFilter: ColorFilter.linearToSrgbGamma(),
-                        image: AssetImage("assets/world.png"),
+                        image: AssetImage("assets/continents_background/$continentTitle.png"),
                         fit: BoxFit.cover
                     )
                 ),
@@ -48,9 +56,9 @@ class WorldState extends State<World> {
                                 child: Row(
                                     children: [
                                         SizedBox(width: 20),
-                                        getContinent('Europe'),
-                                        getContinent('Africa'),
-                                        getContinent('North America'),
+                                        getCountry('Andorra'),
+                                        getCountry('Bosnia and Herzegovina'),
+                                        getCountry('Netherlands'),
                                         SizedBox(width: 20),
                                     ]
                                 )
@@ -62,7 +70,7 @@ class WorldState extends State<World> {
         );
     }
 
-    getContinent(String continentTitle) {
+    getCountry(String countryTitle) {
         return Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Stack(
@@ -84,7 +92,7 @@ class WorldState extends State<World> {
                                     Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                            builder: (context) => ContinentScreen(continentTitle: continentTitle)
+                                            builder: (context) => GameScreen(countryTitle: countryTitle, gameMode: GameMode.EASY)
                                         )
                                     )
                                 },
@@ -92,33 +100,11 @@ class WorldState extends State<World> {
                                     padding: EdgeInsets.all(10),
                                     height: MediaQuery.of(context).size.height - 150,
                                     width: MediaQuery.of(context).size.height - 150,
-                                    child: SvgPicture.asset(
-                                        'assets/svg/$continentTitle.svg',
+                                    child: Image(
+                                        image: AssetImage('assets/shapes/$continentTitle/$countryTitle.png'),
                                         height: MediaQuery.of(context).size.height - 100,
+                                        color: Colors.white,
                                         fit: BoxFit.contain
-                                    )
-                                )
-                            )
-                        )
-                    ),
-                    Positioned(
-                        top: 20,
-                        right: 40,
-                        child: Card(
-                            color: AppTheme.MAIN_COLOR,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)
-                            ),
-                            child: Container(
-                                padding: EdgeInsets.only(left: 20, right: 30),
-                                height: 35,
-                                child: Center(
-                                    child: Text(
-                                        continentTitle,
-                                        style: TextStyle(
-                                            color: AppTheme.THIRD_COLOR,
-                                            fontWeight: FontWeight.bold
-                                        )
                                     )
                                 )
                             )
@@ -127,21 +113,36 @@ class WorldState extends State<World> {
                     Positioned(
                         top: 15,
                         right: 15,
-                        child: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                                color: AppTheme.MAIN_COLOR,
-                                shape: BoxShape.circle
+                        child: Card(
+                            color: AppTheme.MAIN_COLOR,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)
                             ),
-                            child: Center(
-                                child: Text(
-                                    '3/44',
-                                    style: TextStyle(
-                                        color: AppTheme.THIRD_COLOR,
-                                        fontWeight: FontWeight.bold
+                            child: Row(
+                                children: [
+                                    Tooltip(
+                                        message: countryTitle,
+                                        preferBelow: false,
+                                        verticalOffset: 40,
+                                        showDuration: Duration(milliseconds: 500),
+                                        child: Container(
+                                            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                            height: 35,
+                                            constraints: BoxConstraints(maxWidth: 150),
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(8.0)
+                                            ),
+                                            child: Text(
+                                                countryTitle,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                    color: AppTheme.THIRD_COLOR,
+                                                    fontWeight: FontWeight.bold
+                                                )
+                                            )
+                                        )
                                     )
-                                )
+                                ]
                             )
                         )
                     ),
@@ -164,8 +165,12 @@ class WorldState extends State<World> {
     }
 }
 
-class World extends StatefulWidget {
+class Continent extends StatefulWidget {
+    final String continentTitle;
+
+    Continent({ Key? key, required this.continentTitle }) : super(key: key);
+
 
 	@override
-	State createState() => WorldState();
+	State createState() => ContinentState();
 }
